db7c040802a7bc954b98a17f4c0ff0974c9b1e1e
Ing. Marcos Gonzalez <ing.marcosgonzalez1@gmail.com>
2018-05-24 18:31:35 -0500

    Update ejercicio sql-postgre.sql

--- ejercicio sql-postgre.sql
+++ ejercicio sql-postgre.sql
@@ -49,3 +49,48 @@ CREATE TABLE detalles(
   CONSTRAINT prod_fk FOREIGN KEY (id_producto)
   REFERENCES productos(id_producto)
 );
+
+
+SELECT c.nombre AS cliente, e.nombre AS empleado, p.id_pedido FROM cliente c LEFT OUTER JOIN pedido p USING(id_cliente) FULL OUTER JOIN empleado e USING (id_empleado);
+SELECT c.nombre as cliente, e.nombre as empleado, p.id_pedido FROM clientes c FULL OUTER JOIN (pedidos p RIGHT OUTER JOIN empleados e USING (id_empleado)) USING(id_cliente);
+
+SELECT id_producto,descripcion FROM producto WHERE descripcion LIKE 'P%' AND descripcion ILIKE '%As' AND descripcion ILIKE '%TA%';
+
+
+SELECT pro.id_producto, pro.descripcion AS producto, dp.cantidad AS cantidad_pedida 
+  FROM pedido p INNER JOIN cliente c using (id_cliente) 
+  INNER JOIN detalles_pedido dp using(id_pedido) 
+  INNER JOIN producto pro using (id_producto) 
+  WHERE c.nombre = 'PEPE';
+  
+  SELECT id_producto, descripcion 
+    FROM producto 
+    WHERE id_producto NOT IN( SELECT id_producto 
+    FROM cliente c INNER JOIN pedido p USING (id_cliente) 
+    INNER JOIN detalles_pedido dp USING(id_pedido) 
+    WHERE c.nombre = 'PEPE');
+    
+    
+    SELECT id_producto,descripcion FROM producto WHERE precio > ANY ( SELECT precio FROM cliente c INNER JOIN pedido p USING (id_cliente) INNER JOIN detalles_pedido dp USING (id_pedido) INNER JOIN producto pro USING (id_producto) WHERE c.nombre = 'PEPE');
+    
+    SELECT id_producto,descripcion FROM producto WHERE precio > ( SELECT MIN(precio) FROM cliente c INNER JOIN pedido p USING (id_cliente) INNER JOIN detalles_pedido dp USING (id_pedido) INNER JOIN producto pro USING (id_producto) WHERE c.nombre = 'PEPE');
+    
+    SELECT id_pedido, fecha_pedido 
+      FROM pedidos p WHERE NOT EXISTS ( SELECT * FROM detalles_pedido dp WHERE p.id_pedido=dp.id_pedido);
+      
+      SELECT nombre FROM empleado WHERE ciudad = 'MIERES' UNION SELECT nombre FROM cliente WHERE ciudad ='MIERES';
+      
+      SELECT ciudad FROM empleado EXCEPT SELECT ciudad FROM cliente;
+      
+      SELECT id_producto, descripcion 
+        FROM producto EXCEPT SELECT pro.id_producto,pro.descripcion 
+        FROM pedido p inner join cliente c USING (id_cliente) 
+        INNER JOIN detalles_pedido dp USING(id_pedido) 
+        INNER JOIN producto pro USING (id_producto) WHERE c.nombre LIKE 'PEPE';
+        
+        SELECT nombre 
+          FROM cliente c1 WHERE NOT EXISTS ( SELECT id_producto --B: productos pedidos por PEPE FROM cliente c2 inner join pedido p using (id_cliente) INNER JOIN detalles_pedido dp using (id_pedido) WHERE c2.nombre = 'PEPE' EXCEPT SELECT id_producto --A: productos pedidos por cada cliente c1 FROM pedido p inner join detalles_pedido dp USING (id_pedido ) WHERE p.id_cliente = c1.id_cliente);
+      
+      
+  
+  
